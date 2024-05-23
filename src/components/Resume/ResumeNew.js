bb3cf9fab46387edeb0b712531cd07c27442416e
polokang <polokang@gmail.com>
2024-05-24 00:31:03 +1000

    docs: update resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Hunter Zhou - Full Stack Developer May.pdf";
+import pdf from "../../Assets/../Assets/Hunter Zhou - Full Stack Developer.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
